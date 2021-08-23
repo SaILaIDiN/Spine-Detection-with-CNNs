ec754ab49a05d5476bc29e62c622506b09e448b3
Sercan <mario.gomes2@web.de>
2021-08-23 17:33:17 +0200

    Added config files and model files in train_mmdet.py to train four different architectures, namely Cascade-RCNN, VarifocalNet, Generalized Focal Loss and Deformable Detection Transformer (DETR).

--- train_mmdet.py
+++ train_mmdet.py
@@ -2,55 +2,60 @@ import mmcv
 from mmcv import Config
 from mmdet.apis.train import set_random_seed
 from dataset_mmdet import SpineDataset, DATASETS
+# # CASCADE-RCNN
+# cfg = Config.fromfile("references/mmdetection/configs/cascade_rcnn/cascade_rcnn_x101_64x4d_fpn_1x_coco_SPINE.py")
+# cfg = Config.fromfile("references/mmdetection/configs/cascade_rcnn/cascade_rcnn_x101_64x4d_fpn_1x_coco_SPINE_AUG.py")
+# cfg = Config.fromfile("references/mmdetection/configs/cascade_rcnn/cascade_rcnn_x101_32x4d_fpn_1x_coco_SPINE.py")
+# cfg = Config.fromfile("references/mmdetection/configs/cascade_rcnn/cascade_rcnn_r101_fpn_1x_coco_SPINE.py")
+# cfg = Config.fromfile("references/mmdetection/configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco_SPINE.py")
+# cfg = Config.fromfile("references/mmdetection/configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco_SPINE_AUG.py")
 
-cfg = Config.fromfile("references/mmdetection/configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco_SPINE.py")
+# # GENERALIZED FOCAL LOSS
+# cfg = Config.fromfile("references/mmdetection/configs/gfl/gfl_x101_32x4d_fpn_dconv_c4-c5_mstrain_2x_coco_SPINE.py")
+# cfg = Config.fromfile("references/mmdetection/configs/gfl/gfl_x101_32x4d_fpn_dconv_c4-c5_mstrain_2x_coco_SPINE_AUG.py")
+# cfg = Config.fromfile("references/mmdetection/configs/gfl/gfl_x101_32x4d_fpn_mstrain_2x_coco_SPINE.py")
+# cfg = Config.fromfile("references/mmdetection/configs/gfl/gfl_r101_fpn_dconv_c3-c5_mstrain_2x_coco_SPINE.py")
 
-# # These are the main changes in a config file to fit our single class spine dataset
-# cfg.dataset_type = "SpineDataset"
-# cfg.data_root = "data/raw/"
-# cfg.img_norm_cfg.to_rgb = True
+# # VARIFOCALNET
+# cfg = Config.fromfile("references/mmdetection/configs/vfnet/vfnet_x101_64x4d_fpn_mdconv_c3-c5_mstrain_2x_coco_SPINE.py")
+# cfg = Config.fromfile("references/mmdetection/configs/vfnet/vfnet_x101_64x4d_fpn_mdconv_c3-c5_mstrain_2x_coco_SPINE_AUG.py")
 
-# cfg.train_pipeline[2].img_scale = (512, 512)
-# cfg.test_pipeline[1].img_scale = (512, 512)
-#
-# cfg.data.train.pipeline[2].img_scale = (512, 512)
-# cfg.data.test.pipeline[1].img_scale = (512, 512)
-# cfg.data.val.pipeline[1].img_scale = (512, 512)
-
-# cfg.data.samples_per_gpu = 1
-# cfg.data.workers_per_gpu = 1
-#
-# cfg.data.train.type = "SpineDataset"
-# cfg.data.train.ann_file = "data/default_annotations/data_train.csv"
-# cfg.data.train.img_prefix = ""
-#
-# cfg.data.val.type = "SpineDataset"
-# cfg.data.val.ann_file = "data/default_annotations/data_val.csv"
-# cfg.data.val.img_prefix = ""
-#
-# cfg.data.test.type = "SpineDataset"
-# cfg.data.test.ann_file = "data/default_annotations/data_val.csv"
-# cfg.data.test.img_prefix = ""
-#
-# cfg.evaluation.interval = 1
-# cfg.evaluation.metric = 'mAP'
-#
-# cfg.model.roi_head.bbox_head[0].num_classes = 1
-# cfg.model.roi_head.bbox_head[1].num_classes = 1
-# cfg.model.roi_head.bbox_head[2].num_classes = 1
+# # DEFORMABLE DETR
+cfg = Config.fromfile("references/mmdetection/configs/deformable_detr/deformable_detr_twostage_refine_r50_16x2_50e_coco_SPINE.py")
+# cfg = Config.fromfile("references/mmdetection/configs/deformable_detr/deformable_detr_twostage_refine_r50_16x2_50e_coco_SPINE_AUG.py")
 
 cfg.seed = 0
 set_random_seed(0, deterministic=False)
 cfg.gpu_ids = range(1)
 
-# this loads the pretrained weights on COCO dataset into our model
-cfg.load_from = 'references/mmdetection/checkpoints/cascade_rcnn_r50_fpn_1x_coco_20200316-3dc56deb.pth'
+# this loads the pretrained weights on COCO dataset into our model (or resume from a model)
+cfg.resume_from = 'tutorial_exps/epoch_14.pth'
+
+# # CASCADE-RCNN
+# cfg.load_from = 'references/mmdetection/checkpoints/cascade_rcnn_x101_64x4d_fpn_1x_coco_20200515_075702-43ce6a30.pth'
+# cfg.load_from = 'references/mmdetection/checkpoints/cascade_rcnn_x101_32x4d_fpn_1x_coco_20200316-95c2deb6.pth'
+# cfg.load_from = 'references/mmdetection/checkpoints/cascade_rcnn_r101_fpn_1x_coco_20200317-0b6a2fbf.pth'
+# cfg.load_from = 'references/mmdetection/checkpoints/cascade_rcnn_r50_fpn_1x_coco_20200316-3dc56deb.pth'
+
+# # GENERALIZED FOCAL LOSS
+# cfg.load_from = 'references/mmdetection/checkpoints/gfl_x101_32x4d_fpn_dconv_c4-c5_mstrain_2x_coco_20200630_102002-14a2bf25.pth'
+# cfg.load_from = 'references/mmdetection/checkpoints/gfl_x101_32x4d_fpn_mstrain_2x_coco_20200630_102002-50c1ffdb.pth'
+# cfg.load_from = 'references/mmdetection/checkpoints/gfl_r101_fpn_dconv_c3-c5_mstrain_2x_coco_20200630_102002-134b07df.pth'
+
+# # VARIFOCALNET
+# cfg.load_from = 'references/mmdetection/checkpoints/vfnet_x101_64x4d_fpn_mdconv_c3-c5_mstrain_2x_coco_20201027pth-b5f6da5e.pth'
+
+# # DEFORMABLE DETR
+# cfg.load_from = 'references/mmdetection/checkpoints/deformable_detr_twostage_refine_r50_16x2_50e_coco_20210419_220613-9d28ab72.pth'
+
+
 # directory for the trained model weights
 cfg.work_dir = 'tutorial_exps'
 
-# cfg.optimizer.lr = 0.00002
+cfg.optimizer.lr = 0.00005
 cfg.lr_config.warmup = None
-cfg.runner.max_epochs = 10
+# cfg.lr_config.warmup = "linear"
+cfg.runner.max_epochs = 15
 
 # We can initialize the logger for training and have a look
 # at the final config used for training
