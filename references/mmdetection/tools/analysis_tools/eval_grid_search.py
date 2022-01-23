d4379f24116e0134e677e18c25f1440bdba0d802
Sercan <mario.gomes2@web.de>
2022-01-23 14:36:54 +0100

    Added evaluation of Def-DETR in eval_grid_search.py.

--- references/mmdetection/tools/analysis_tools/eval_grid_search.py
+++ references/mmdetection/tools/analysis_tools/eval_grid_search.py
@@ -8,7 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def load_json_log_filter_top_N(json_log, mode=None, top_N=1, lr=None, momentum=None, weight_decay=None):
+def load_json_log_filter_top_N(json_log, mode=None, top_N=1, lr=None, momentum=None, weight_decay=None, dropout=None):
     """ Similar way to read and manipulate json file entries such as load_json_logs().
         This function filters non-relevant rows of the json file and return the top three entries, with loss, and epoch.
 
@@ -47,6 +47,8 @@ def load_json_log_filter_top_N(json_log, mode=None, top_N=1, lr=None, momentum=N
             df_top_N["lr"] = lr
             df_top_N["momentum"] = momentum
             df_top_N["weight_decay"] = weight_decay
+            if dropout is not None:
+                df_top_N["dropout"] = dropout
             list_dict_top_N = df_top_N.reset_index().to_dict('records')
 
     return list_dict_top_N
@@ -78,15 +80,50 @@ def prep_Cascade_or_VFNet_with_SGD_logs_top_N(test_content, list_train_csv, list
     return df_total_top_N
 
 
+def prep_Def_DETR_with_SGD_logs_top_N(test_content, list_train_csv, list_special_term, list_da_modes,
+                                      list_model_type, list_learning_rate, list_weight_decay,
+                                      list_warm_up, list_momentum, list_dropout, top_N):
+    total_list_of_top_N = []
+    for lr in list_learning_rate:
+        for da_mode in list_da_modes:
+            for model_type in list_model_type:
+                for train_csv, special_term in zip(list_train_csv, list_special_term):
+                    for warm_up in list_warm_up:
+                        for dropout in list_dropout:
+                            for momentum in list_momentum:
+                                for weight_decay in list_weight_decay:
+                                    run_name = 'lr_' + str(lr) + '_warmup_' + str(warm_up) + \
+                                               '_dropout_' + str(dropout) + '_momentum_' + str(momentum) +\
+                                               '_L2_' + str(weight_decay) + str(special_term)
+                                    test_path = \
+                                        f"{model_type}_Plot_Analysis/{test_content.split('/')[0]}/{da_mode}/{run_name}"
+                                    log_path = test_path + "/None.log.json"
+                                    list_of_top_N = load_json_log_filter_top_N(log_path, mode="Val", top_N=top_N,
+                                                                               lr=lr, momentum=momentum,
+                                                                               weight_decay=weight_decay,
+                                                                               dropout=dropout)
+                                    [total_list_of_top_N.append(list_of_top_N_sub) for list_of_top_N_sub in list_of_top_N]
+    df_total_top_N = pd.DataFrame(total_list_of_top_N)
+    df_total_top_N = df_total_top_N.sort_values(by=['loss'], ascending=True)
+
+    return df_total_top_N
+
 def scatter_plot_loss_by_single_param(df, key_param, xlabel, ylabel, title=None, output=None, custom_dict=None):
     """ """
-    plt.xlabel(xlabel, fontsize=15)
-    plt.ylabel(ylabel, fontsize=16)
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
-    # ax = plt.gca()
+    plt.xlabel(xlabel, fontsize=20)
+    plt.ylabel(ylabel, fontsize=20)
+    plt.xticks(fontsize=19)
+    plt.yticks(fontsize=19)
+    plt.locator_params(axis='y', nbins=5)
+    ax = plt.gca()
     # ax.set_xlim(xmin=0, xmax=2300)
-    # ax.set_ylim(bottom=0, ymax=3.25)
+    # ax.set_ylim(bottom=0.3, ymax=0.6)  # for Cascade-RCNN
+    # ax.set_ylim(bottom=1.62, ymax=1.675)  # for VFNet
+    ax.set_ylim(bottom=1.55, ymax=2.7)  # for VFNet
+    # ax.set_ylim(bottom=6.5, ymax=7.5)  # for Def-DETR
+    # ax.set_ylim(bottom=6.5, ymax=15)  # for Def-DETR
+
+
     if custom_dict is not None:
         # custom order needed, because of str formatting of small decimals to Xe-YZ style
         df = df.sort_values(by=[key_param], ascending=True, key=lambda x: x.map(custom_dict))
@@ -118,15 +155,15 @@ if __name__ == "__main__":
     # example_val_loss_top_N = load_json_log_filter_top_N(log_path, mode="Val", top_N=5)
     # print(example_val_loss_top_N)
 
-    # # Test prep_Cascade_or_VFNet_with_SGD_logs_top_N()
-    # for Cascade
+    # Test prep_Cascade_or_VFNet_with_SGD_logs_top_N()
+    # # for Cascade
     # test_content = "02_Test_Grid_Search/some_folder"
     # list_train_csv = [f"data/default_annotations/train.csv" for i in range(0, 1)]
     # list_special_term = [f"_run_{i + 1}" for i in range(0, 1)]
     # list_da_modes = ["mixed_DA"]
     # list_model_type = ["Cascade_RCNN"]
     # list_learning_rate = ['0.01', '0.001', '0.0001', '1e-05', '1e-06']
-    # list_weight_decay = ['0.03', '0.0003', '3e-06']
+    # list_weight_decay = ['0.3', '0.03', '0.0003', '3e-06']
     # list_warm_up = [None]
     # list_momentum = ['0.0', '0.3', '0.6', '0.9']
 
@@ -143,8 +180,25 @@ if __name__ == "__main__":
 
     df_total_top_1 = prep_Cascade_or_VFNet_with_SGD_logs_top_N(
         test_content, list_train_csv, list_special_term, list_da_modes, list_model_type, list_learning_rate,
-        list_weight_decay, list_warm_up, list_momentum, top_N=2)
+        list_weight_decay, list_warm_up, list_momentum, top_N=1)
     print(df_total_top_1.to_string())
+
+    # # for Def-DETR
+    # test_content = "02_Test_Grid_Search/some_folder"
+    # list_train_csv = [f"data/default_annotations/train.csv" for i in range(0, 1)]
+    # list_special_term = [f"_run_{i + 1}" for i in range(0, 1)]
+    # list_da_modes = ["mixed_DA"]
+    # list_model_type = ["Def_DETR"]
+    # list_learning_rate = ['0.001', '0.0001', '1e-05', '1e-06']
+    # list_weight_decay = ['0.03', '0.0003', '3e-06']
+    # list_warm_up = [None]
+    # list_momentum = ['0.0', '0.3', '0.6', '0.9']
+    # list_dropout = ['0.1']
+    #
+    # df_total_top_1 = prep_Def_DETR_with_SGD_logs_top_N(
+    #     test_content, list_train_csv, list_special_term, list_da_modes, list_model_type, list_learning_rate,
+    #     list_weight_decay, list_warm_up, list_momentum, list_dropout, top_N=4)
+    # print(df_total_top_1.to_string())
 
     # # Test scatter_plot_loss_by_single_param()
     scatter_plot_loss_by_single_param(df_total_top_1, "momentum", xlabel="momentum", ylabel="val loss",
@@ -155,4 +209,4 @@ if __name__ == "__main__":
                                       custom_dict={'0.01': 0, '0.001': 1, '0.0001': 2, '1e-05': 3, '1e-06': 4})
     scatter_plot_loss_by_single_param(df_total_top_1, "weight_decay", xlabel="weight decay", ylabel="val loss",
                                       output="scatter_weight_decay_loss",
-                                      custom_dict={'0.03': 0, '0.0003': 1, '3e-06': 2})
+                                      custom_dict={'0.3': 0, '0.03': 1, '0.0003': 2, '3e-06': 3})
