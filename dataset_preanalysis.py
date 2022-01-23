ee3d7b855cf623e6e051a68c7cd840b2e06389a8
Sercan <mario.gomes2@web.de>
2022-01-23 14:40:11 +0100

    Added computation of mean and std for histogram of spines per image in dataset_preanalysis.py.

--- dataset_preanalysis.py
+++ dataset_preanalysis.py
@@ -71,27 +71,38 @@ def number_of_spines_per_image(dir_csv):
     return dist_spines_by_image
 
 
+def compute_mean_and_std(dict_counter):
+    """ Turn a dict of Counter into a list and compute mean and standard deviation """
+    list_values = []
+    for value in dict_counter.values():
+        list_values.append(value)
+    mean = np.mean(list_values)
+    std = np.std(list_values)
+    median = np.median(list_values)
+    return mean, std, median
+
+
 def hist_plot_stacks(X, xlabel, ylabel, output=None):
-    plt.xlabel(xlabel, fontsize=15)
-    plt.ylabel(ylabel, fontsize=16)
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
+    plt.xlabel(xlabel, fontsize=20)
+    plt.ylabel(ylabel, fontsize=20)
+    plt.xticks(fontsize=19)
+    plt.yticks(fontsize=19)
     ax = plt.gca()
 
-    ax.set_ylim(bottom=0, ymax=4.5)  # for train
-    plt.locator_params(axis='x', nbins=10)  # for train
-    plt.locator_params(axis='y', nbins=5)  # for train
-    plt.hist(X, rwidth=0.75, edgecolor='black', alpha=1.0, bins=63, align='left')  # train
+    # ax.set_ylim(bottom=0, ymax=4.5)  # for train
+    # plt.locator_params(axis='x', nbins=10)  # for train
+    # plt.locator_params(axis='y', nbins=5)  # for train
+    # plt.hist(X, rwidth=0.75, edgecolor='black', alpha=1.0, bins=63, align='left')  # train
 
     # ax.set_ylim(bottom=0, ymax=2.5)  # for val
     # plt.locator_params(axis='x', nbins=14)  # for val
     # plt.locator_params(axis='y', nbins=3)  # for val
     # plt.hist(X, rwidth=0.75, edgecolor='black', alpha=1.0, bins=15, align='left')  # val
 
-    # ax.set_ylim(bottom=0, ymax=2.5)  # for test
-    # plt.locator_params(axis='x', nbins=9)  # for test
-    # plt.locator_params(axis='y', nbins=3)  # for test
-    # plt.hist(X, rwidth=0.75, edgecolor='black', alpha=1.0, bins=8, align='left')  # test
+    ax.set_ylim(bottom=0, ymax=2.5)  # for test
+    plt.locator_params(axis='x', nbins=9)  # for test
+    plt.locator_params(axis='y', nbins=3)  # for test
+    plt.hist(X, rwidth=0.75, edgecolor='black', alpha=1.0, bins=8, align='left')  # test
 
     plt.tight_layout()
     if output is None:
@@ -103,22 +114,22 @@ def hist_plot_stacks(X, xlabel, ylabel, output=None):
 
 
 def hist_plot_spines(X, xlabel, ylabel, output=None):
-    plt.xlabel(xlabel, fontsize=15)
-    plt.ylabel(ylabel, fontsize=16)
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
+    plt.xlabel(xlabel, fontsize=20)
+    plt.ylabel(ylabel, fontsize=20)
+    plt.xticks(fontsize=19)
+    plt.yticks(fontsize=19)
 
-    plt.locator_params(axis='x', nbins=10)  # for train
-    plt.locator_params(axis='y', nbins=5)  # for train
-    plt.hist(X, rwidth=0.75, edgecolor='black', alpha=1.0, bins=25, align='left')  # train
+    # plt.locator_params(axis='x', nbins=10)  # for train
+    # plt.locator_params(axis='y', nbins=5)  # for train
+    # plt.hist(X, rwidth=0.75, edgecolor='black', alpha=1.0, bins=25, align='left')  # train
 
     # plt.locator_params(axis='x', nbins=10)  # for val
     # plt.locator_params(axis='y', nbins=4)  # for val
     # plt.hist(X, rwidth=0.75, edgecolor='black', alpha=1.0, bins=14, align='left')  # val
 
-    # plt.locator_params(axis='x', nbins=10)  # for test
-    # plt.locator_params(axis='y', nbins=6)  # for test
-    # plt.hist(X, rwidth=0.75, edgecolor='black', alpha=1.0, bins=18, align='left')  # test
+    plt.locator_params(axis='x', nbins=10)  # for test
+    plt.locator_params(axis='y', nbins=6)  # for test
+    plt.hist(X, rwidth=0.75, edgecolor='black', alpha=1.0, bins=18, align='left')  # test
 
     plt.tight_layout()
     if output is None:
@@ -168,12 +179,20 @@ if __name__ == '__main__':
     dist_stacks_train = size_of_stacks(train_csv_path)
     dist_stacks_val = size_of_stacks(val_csv_path)
     dist_stacks_test = size_of_stacks(test_csv_path)
-    # hist_plot_stacks(X=np.array(list(dist_stacks_val.values())).astype(float),
-    #                  xlabel="stack size", ylabel="count", output="hist_stack_size_val")
+    hist_plot_stacks(X=np.array(list(dist_stacks_test.values())).astype(float),
+                     xlabel="stack size", ylabel="count", output="hist_stack_size_test")
 
     # # # 4
     dist_spines_train = number_of_spines_per_image(train_csv_path)
     dist_spines_val = number_of_spines_per_image(val_csv_path)
     dist_spines_test = number_of_spines_per_image(test_csv_path)
-    # hist_plot_spines(X=np.array(list(dist_spines_test.values())).astype(float),
-    #                  xlabel="spines per image", ylabel="count", output="hist_spine_number_test")
+    hist_plot_spines(X=np.array(list(dist_spines_test.values())).astype(float),
+                     xlabel="spines per image", ylabel="count", output="hist_spine_number_test")
+
+    # # # 5
+    mean_spines_train, std_spines_train, median_spines_train = compute_mean_and_std(dist_spines_train)
+    print(mean_spines_train, std_spines_train, median_spines_train)
+    mean_spines_val, std_spines_val, median_spines_val = compute_mean_and_std(dist_spines_val)
+    print(mean_spines_val, std_spines_val, median_spines_val)
+    mean_spines_test, std_spines_test, median_spines_test = compute_mean_and_std(dist_spines_test)
+    print(mean_spines_test, std_spines_test, median_spines_test)
