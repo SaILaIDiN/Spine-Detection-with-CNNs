d53019e21a15782e16a2be8c0dced08b8bdf620b
legnaib <leg.naib@gmail.com>
2023-01-30 22:39:54 +0100

    Update version

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ def readme():
 if __name__ == "__main__":
     setup(
         name="spine-detection",
-        version="0.0.1",
+        version="1.0.0",
         description="Dendritic Spine Detection module",
         long_description=readme(),
         long_description_content_type="text/markdown",
-        author="OpenMMLab",
-        author_email="openmmlab@gmail.com",
+        author="Sercan Alipek, Fabian W. Vogel",
+        author_email="alipek@physik.uni-frankfurt.de, fabian@vogel-nest.de",
         keywords="computer vision, object detection, dendritic spine",
         url="https://github.com/SaILaIDiN/Spine-Detection-with-CNNs",
         packages=find_packages(where="src"),
@@ -25,15 +25,6 @@ if __name__ == "__main__":
         package_data={"spine_detection": ["configs/*"]},
         include_package_data=True,
         license="Apache License 2.0",
-        # setup_requires=parse_requirements("requirements/build.txt"),
-        # tests_require=parse_requirements("requirements/tests.txt"),
-        # install_requires=parse_requirements("requirements/runtime.txt"),
-        # extras_require={
-        #     "all": parse_requirements("requirements.txt"),
-        #     "tests": parse_requirements("requirements/tests.txt"),
-        #     "build": parse_requirements("requirements/build.txt"),
-        #     "optional": parse_requirements("requirements/optional.txt"),
-        # },
         ext_modules=[],
         zip_safe=False,
     )
