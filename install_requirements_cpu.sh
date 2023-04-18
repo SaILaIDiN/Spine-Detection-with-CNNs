20c51f176ff09cbf167effb819726281f4bd48c1
legnaib <leg.naib@gmail.com>
2023-04-18 19:12:56 +0200

    install cpu versions of torch

---
+++ install_requirements_cpu.sh
@@ -0,0 +1,8 @@
+pip install --no-cache-dir --upgrade pip wheel setuptools
+pip install torch torchvision torchaudio
+pip install -r requirements.txt
+pip install openmim
+
+# On Python 3.9 no pre-built package exists and it will be compiled, taking ~10 minutes
+mim install mmcv-full==1.6
+pip install .
